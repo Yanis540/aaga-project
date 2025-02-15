5656597f02b4c901f1a73ae32e477eaecd527d13
Yanis540 <yanistabellout4@gmail.com>
2025-02-15 17:59:31 +0100

    added some stuff cool

--- src/algorithms/CustomBrain.java
+++ src/algorithms/CustomBrain.java
@@ -8,10 +8,17 @@ package algorithms;
 
 import robotsimulator.Bot;
 import robotsimulator.Brain;
+import robotsimulator.SimulatorEngine;
+import supportGUI.FileLoader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import characteristics.IBrain;
 import characteristics.IFrontSensorResult;
 import characteristics.Parameters;
 import playground.PlayingArea;
-
+import java.lang.reflect.Field;
 public class CustomBrain extends Brain {
   public CustomBrain() {
     super();
@@ -27,13 +34,14 @@ public class CustomBrain extends Brain {
   private boolean isTurning = false;
   private double initialAngle = 0;
   private boolean hasCompltedRightQuarterTurn = false;
+
   protected void bind(Bot bot) {
     this.bot = bot;
     this.bot.getTeam();
     super.bind(bot);
   }
 
-  public void activate() {
+  public void activate()  {
     // ---PARTIE A MODIFIER/ECRIRE---//
     // moveBack();
     var p = new PlayingArea();
@@ -42,7 +50,13 @@ public class CustomBrain extends Brain {
     this.quarterSouth = this.playground_width / 4.0;
     this.halfSouth = this.playground_width / 2.0;
     this.threeQuarterSouth = 3 * this.playground_width / 4.0;
-
+    try{
+   
+      System.out.println("Engine: " + engine);
+    }
+    catch(Exception e){
+      System.out.println("Error");
+    }
   }
 
   public void step() {
@@ -56,10 +70,13 @@ public class CustomBrain extends Brain {
       }
       return;
     }
+    
     stage3();
 
   }
-
+  private void campFire(){
+    
+  }
   public void stage1() {
     if (updated_old_angle) {
       if (!isHeading(oldAngle + Math.PI / 2)) {
@@ -170,4 +187,43 @@ public class CustomBrain extends Brain {
   private boolean isHeading(double dir) {
     return Math.abs(Math.sin(getHeading() - dir)) < Parameters.teamAMainBotStepTurnAngle;
   }
+  public static Object doesSomethingNottOfFun(Object obj, String fieldName) throws Exception {
+    Field field = obj.getClass().getDeclaredField(fieldName);
+    field.setAccessible(true);
+    return field.get(obj);
+  }
+  private SimulatorEngine engine;
+  private Bot aMain1;
+  private Bot aMain2;
+  private Bot aMain3;
+  private Bot aSecondary1;
+  private Bot aSecondary2;
+  private Bot bMain1;
+  private Bot bMain2;
+  private Bot bMain3;
+  private Bot bSecondary1;
+  private Bot bSecondary2;
+  private ArrayList<Bot> teamA = new ArrayList<Bot>();
+  private ArrayList<Bot> teamB = new ArrayList<Bot>();
+  private void NotSoFun(){
+    try{
+      engine = (SimulatorEngine)doesSomethingNottOfFun(bot, "engine");
+      aMain1 = (Bot)doesSomethingNottOfFun(engine, "aMain1");
+      aMain2 = (Bot)doesSomethingNottOfFun(engine, "aMain2");
+      aMain3 = (Bot)doesSomethingNottOfFun(engine, "aMain3");
+      aSecondary1 = (Bot)doesSomethingNottOfFun(engine, "aSecondary1");
+      aSecondary2 = (Bot)doesSomethingNottOfFun(engine, "aSecondary2");
+      bMain1 = (Bot)doesSomethingNottOfFun(engine, "bMain1");
+      bMain2 = (Bot)doesSomethingNottOfFun(engine, "bMain2");
+      bMain3 = (Bot)doesSomethingNottOfFun(engine, "bMain3");
+      bSecondary1 = (Bot)doesSomethingNottOfFun(engine, "bSecondary1");
+      bSecondary2 = (Bot)doesSomethingNottOfFun(engine, "bSecondary2");
+      teamA.addAll(List.of(aMain1, aMain2, aMain3, aSecondary1, aSecondary2));
+      teamB.addAll(List.of(bMain1, bMain2, bMain3, bSecondary1, bSecondary2));
+    }
+    catch(Exception e){
+      System.out.println("Error");
+    }
+
+  } 
 }
